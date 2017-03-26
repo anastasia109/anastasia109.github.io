0112b0ea8647e12c13b6049b4689226c65df9795
anastasia109 <anastasia18106@gmail.com>
2017-03-26 23:01:43 +0300

    Removed old files

--- js/clock.js
+++
@@ -1,4 +0,0 @@
-var d = new Date();
-document.getElementById("day").innerHTML = d.getDate();
-document.getElementById("month").innerHTML = d.getMonth();
-document.getElementById("year").innerHTML = d.getFullYear();
